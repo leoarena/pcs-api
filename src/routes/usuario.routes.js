168800b81275b8eca7f5ba8342420c9c7294a515
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-17 20:03:20 -0300

    feat: updateOneUsuario

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,14 +1,17 @@
 const {
   createOneUsuario,
   loginUsuario,
+  updateOneUsuario,
 } = require("../controllers/usuario.controller");
 const { Router } = require("express");
+const { auth } = require("../middlewares/auth");
 
 class UsuarioRouter {
   routesFromUsuario() {
     const usuarioRoutes = Router();
     usuarioRoutes.post("/usuarios", createOneUsuario);
     usuarioRoutes.post("/usuarios/login", loginUsuario);
+    usuarioRoutes.patch("/usuarios/:identificador", auth, updateOneUsuario);
     return usuarioRoutes;
   }
 }
