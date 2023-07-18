70234f60bb0a3b990beedbb0a26c923bd3f321d8
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-18 15:41:14 -0300

    feat: updateOneDeposito

--- src/controllers/deposito.controller.js
+++ src/controllers/deposito.controller.js
@@ -130,6 +130,168 @@ class DepositoController {
       });
     }
   }
+
+  async updateOneDeposito(request, response) {
+    try {
+      const { identificador } = request.params;
+      const {
+        nomeFantasia,
+        email,
+        telefone,
+        celular,
+        cep,
+        logradouro,
+        numero,
+        bairro,
+        cidade,
+        estado,
+        complemento,
+        latitude,
+        longitude,
+      } = request.body;
+
+      const deposito = await Deposito.findOne({ where: { identificador } });
+      if (!deposito)
+        return response
+          .status(404)
+          .send({ message: "Depósito não encontrado." });
+
+      if (
+        !nomeFantasia &&
+        !email &&
+        !telefone &&
+        !celular &&
+        !cep &&
+        !logradouro &&
+        !numero &&
+        !bairro &&
+        !cidade &&
+        !estado &&
+        !complemento &&
+        !latitude &&
+        !longitude
+      )
+        return response
+          .status(400)
+          .send({ message: "Pelo menos um dos campos deve ser editado." });
+
+      const nomeFantasiaValido =
+        typeof nomeFantasia === "string" || typeof nomeFantasia === "undefined";
+      if (!nomeFantasiaValido)
+        return response.status(400).send({
+          message: "O campo nome fantasia precisa ser do tipo string.",
+        });
+
+      const emailValido =
+        typeof email === "string" || typeof email === "undefined";
+      if (!emailValido)
+        return response.status(400).send({
+          message: "O campo email precisa ser do tipo string.",
+        });
+
+      const telefoneValido =
+        typeof telefone === "string" || typeof telefone === "undefined";
+      if (!telefoneValido)
+        return response.status(400).send({
+          message: "O campo telefone precisa ser do tipo string.",
+        });
+
+      const celularValido =
+        typeof celular === "string" || typeof celular === "undefined";
+      if (!celularValido)
+        return response.status(400).send({
+          message: "O campo celular precisa ser do tipo string.",
+        });
+
+      const cepValido = typeof cep === "string" || typeof cep === "undefined";
+      if (!cepValido)
+        return response.status(400).send({
+          message: "O campo cep precisa ser do tipo string.",
+        });
+
+      const logradouroValido =
+        typeof logradouro === "string" || typeof logradouro === "undefined";
+      if (!logradouroValido)
+        return response.status(400).send({
+          message: "O campo logradouro precisa ser do tipo string.",
+        });
+
+      const numeroValido =
+        typeof numero === "string" || typeof numero === "undefined";
+      if (!numeroValido)
+        return response.status(400).send({
+          message: "O campo numero precisa ser do tipo string.",
+        });
+
+      const bairroValido =
+        typeof bairro === "string" || typeof bairro === "undefined";
+      if (!bairroValido)
+        return response.status(400).send({
+          message: "O campo bairro precisa ser do tipo string.",
+        });
+
+      const cidadeValido =
+        typeof cidade === "string" || typeof cidade === "undefined";
+      if (!cidadeValido)
+        return response.status(400).send({
+          message: "O campo cidade precisa ser do tipo string.",
+        });
+
+      const estadoValido =
+        typeof estado === "string" || typeof estado === "undefined";
+      if (!estadoValido)
+        return response.status(400).send({
+          message: "O campo estado precisa ser do tipo string.",
+        });
+
+      const complementoValido =
+        typeof complemento === "string" || typeof complemento === "undefined";
+      if (!complementoValido)
+        return response.status(400).send({
+          message: "O campo complemento precisa ser do tipo string.",
+        });
+
+      const latitudeValido =
+        typeof latitude === "string" || typeof latitude === "undefined";
+      if (!latitudeValido)
+        return response.status(400).send({
+          message: "O campo latitude precisa ser do tipo string.",
+        });
+
+      const longitudeValido =
+        typeof longitude === "string" || typeof longitude === "undefined";
+      if (!longitudeValido)
+        return response.status(400).send({
+          message: "O campo longitude precisa ser do tipo string.",
+        });
+
+      await deposito.update(
+        {
+          nomeFantasia,
+          email,
+          telefone,
+          celular,
+          cep,
+          logradouro,
+          numero,
+          bairro,
+          cidade,
+          estado,
+          complemento,
+          latitude,
+          longitude,
+        },
+        { where: { identificador } }
+      );
+
+      return response.status(204).send();
+    } catch (error) {
+      return response.status(400).send({
+        message: "Não foi possível atualizar o depósito.",
+        cause: error.message,
+      });
+    }
+  }
 }
 
 module.exports = new DepositoController();
--- src/routes/deposito.routes.js
+++ src/routes/deposito.routes.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const { auth } = require("../middlewares/auth");
-const { createOneDeposito } = require("../controllers/deposito.controller");
+const {
+  createOneDeposito,
+  updateOneDeposito,
+} = require("../controllers/deposito.controller");
 
 class DepositoRouter {
   routesFromDeposito() {
     const depositoRoutes = Router();
     depositoRoutes.post("/depositos", auth, createOneDeposito);
+    depositoRoutes.patch("/depositos/:identificador", auth, updateOneDeposito);
     return depositoRoutes;
   }
 }
