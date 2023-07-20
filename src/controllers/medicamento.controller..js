c226c40a33363b1d586050b6fe3a072fd642811f
Leonardo Arena <leonardoarena7@gmail.com>
2023-07-20 19:33:15 -0300

    feat: updateOneMedicamento

--- src/controllers/medicamento.controller..js
+++ src/controllers/medicamento.controller..js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { Medicamento } = require("../models/medicamento");
 
 class MedicamentoController {
@@ -79,6 +80,63 @@ class MedicamentoController {
     } catch (error) {
       return response.status(400).send({
         message: "Não foi possível cadastrar o medicamento.",
+        cause: error.message,
+      });
+    }
+  }
+
+  async updateOneMedicamento(request, response) {
+    try {
+      const { descricao, precoUnitario, quantidade } = request.body;
+      const { identificador } = request.params;
+
+      const medicamento = await Medicamento.findOne({
+        where: { identificador },
+      });
+
+      if (!medicamento)
+        return response
+          .status(404)
+          .send({ message: "Medicamento não encontrado." });
+
+      if (!descricao && !precoUnitario && !quantidade)
+        return response
+          .status(400)
+          .send({ message: "Pelo menos um dos campos deve ser editado." });
+
+      const descricaoValida =
+        typeof descricao === "string" || typeof descricao === "undefined";
+      if (!descricaoValida)
+        return response
+          .status(400)
+          .send({ message: "O campo descrição precisa ser do tipo string." });
+
+      const precoUnitarioValido =
+        typeof precoUnitario === "number" ||
+        typeof precoUnitario === "undefined";
+      if (!precoUnitarioValido)
+        return response.status(400).send({
+          message: "O campo preço unitário precisa ser do tipo number.",
+        });
+
+      const quantidadeValida =
+        typeof quantidade === "number" || typeof quantidade === "undefined";
+      if (!quantidadeValida)
+        return response
+          .status(400)
+          .send({ message: "O campo quantidade precisa ser do tipo number." });
+
+      await medicamento.update(
+        { descricao, precoUnitario, quantidade },
+        { where: { identificador } }
+      );
+
+      return response
+        .status(200)
+        .send({ message: "Medicamento atualizado com sucesso.", medicamento });
+    } catch (error) {
+      return response.status(400).send({
+        message: "Não foi possível atualizar o medicamento.",
         cause: error.message,
       });
     }
--- src/routes/medicamento.routes.js
+++ src/routes/medicamento.routes.js
@@ -2,12 +2,18 @@ const { Router } = require("express");
 const { auth } = require("../middlewares/auth");
 const {
   createOneMedicamento,
+  updateOneMedicamento,
 } = require("../controllers/medicamento.controller.");
 
 class MedicamentoRouter {
   routesFromMedicamento() {
     const medicamentoRoutes = Router();
     medicamentoRoutes.post("/medicamentos", auth, createOneMedicamento);
+    medicamentoRoutes.patch(
+      "/medicamentos/:identificador",
+      auth,
+      updateOneMedicamento
+    );
     return medicamentoRoutes;
   }
 }
